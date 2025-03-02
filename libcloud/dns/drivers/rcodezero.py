0d748c85a18ff9f6004e7d93c6103948adc14322
Tomaz Muraus <tomaz@tomaz.me>
2025-03-02 14:41:29 +0100

    Reformat code with the latest version of black.

--- libcloud/dns/drivers/rcodezero.py
+++ libcloud/dns/drivers/rcodezero.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-    RcodeZero DNS Driver
+RcodeZero DNS Driver
 """
 import re
 import json
